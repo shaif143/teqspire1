6d518b04d966b44a517902da76a256d67d715932
Shaif Ahamed Tamim <109494821+shaif143@users.noreply.github.com>
2025-03-16 15:41:56 +0600

    s

--- app/page.js
+++ app/page.js
@@ -7,47 +7,38 @@ import Faq from "./components/faq";
 import Pricing from "./components/pricing";
 import Footer from "./components/footer";
 
+
 export default function App() {
   return (
-    <div className="bg-[#e3eff4] text-black">
-      {/* Sticky Navbar with Spacing Fix */}
-      <div className="sticky top-0 z-50">
-        <NavBar />
-      </div>
-
-      {/* Prevent Overlapping */}
-      <div className="pt-[80px]">
+    <>
+      <div className="bg-[#e3eff4] text-black">
+        <div className="sticky top-0 z-50">
+          <NavBar></NavBar>
+        </div>
         <div id="home">
           <Home />
         </div>
-
         <div id="projects">
           <Project />
         </div>
 
-        {/* Ensure Services Section is Positioned Correctly */}
-        <div id="services" className="relative min-h-screen">
-          <Services />
-        </div>
-
+        <div id="services">
+            <Services />
+        </div> 
+        
         <div id="about">
           <About />
         </div>
-
-        {/* Fix White Background */}
-        <div id="faq" className="bg-white">
+        <div id="faq" className="bg-[#FFFF]">
           <Faq />
         </div>
-
         <div id="pricing">
           <Pricing />
         </div>
-
-        {/* Fix White Background */}
-        <div className="bg-white text-black">
+        <div className="bg-[#FFFF] text-black">
           <Footer />
         </div>
       </div>
-    </div>
+    </>
   );
 }
